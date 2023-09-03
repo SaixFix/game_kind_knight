31d7a53f0180b7382716ebb75575db286554ff80
Maxim <108695003+SaixFix@users.noreply.github.com>
2023-09-03 17:34:47 +0300

    Add more settings param for tiled map and world settings

--- settings.py
+++ settings.py
@@ -1,4 +1,23 @@
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-SCREEN_TITLE = "Kind knight"
-FULL_SCREEN = False
+# Параметры окна
+SCREEN_WIDTH = 1920  # ширина
+SCREEN_HEIGHT = 1080  # высота
+SCREEN_TITLE = "Kind knight"  # название
+FULL_SCREEN = False  # полный экран
+
+# Размеры
+TILE_SPRITE_SCALING = 0.5  # размер плитки
+PLAYER_SCALING = 0.6  # размер игрока
+SPRITE_PIXEL_SIZE = 128  # размер спрайтов
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SPRITE_SCALING  # Размер сетки
+
+# Сколько пикселей оставить в качестве минимального поля между символами
+# и краем экрана.
+VIEWPORT_MARGIN_TOP = 60  # Верх
+VIEWPORT_MARGIN_BOTTOM = 60  # Низ
+VIEWPORT_RIGHT_MARGIN = 270  # Право
+VIEWPORT_LEFT_MARGIN = 270  # Лево
+
+# Физика
+MOVEMENT_SPEED = 5  # Скорость передвижения
+JUMP_SPEED = 23  # скорость прыжка
+GRAVITY = 1.1  # сила гравитации/притяжения
