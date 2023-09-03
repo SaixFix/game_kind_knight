2c8e0068008d6d6f56407853aae41f82f877a832
Maxim <108695003+SaixFix@users.noreply.github.com>
2023-09-03 21:31:20 +0300

    Rework MyGame class for use tiled map with multi-layers capability

--- main.py
+++ main.py
@@ -17,202 +17,227 @@ class MyGame(arcade.Window):
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
 
-        # Tilemap Object
+        # Our TileMap Object
         self.tile_map = None
 
-        # Sprite lists
-        self.player_list = None
+        # Our Scene Object
+        self.scene = None
 
-        # Настройки персонажа
-        self.score = 0
+        # Separate variable that holds the player sprite
         self.player_sprite = None
 
+        # Our physics engine
         self.physics_engine = None
-        self.view_left = 0
-        self.view_bottom = 0
-        self.end_of_map = 0
-        self.game_over = False
-        self.last_time = None
-        self.frame_count = 0
-        self.fps_message = None
 
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
+
+        # A Camera that can be used to draw GUI elements
+        self.gui_camera = None
+
+        # Keep track of the score
+        self.score = 0
+
+        # Do we need to reset the score?
+        self.reset_score = True
+
+        # Where is the right edge of the map?
+        self.end_of_map = 0
+
+        # Level
         self.level = 1
-        self.max_level = 2
+
+        # Load sounds
+        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+        self.game_over = arcade.load_sound(":resources:sounds/gameover1.wav")
 
     def setup(self):
         """Настройте игру здесь. Вызовите эту функцию, чтобы перезапустить игру."""
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
+        # Set up the Cameras
+        self.camera = arcade.Camera(self.width, self.height)
+        self.gui_camera = arcade.Camera(self.width, self.height)
 
-        # Настройки для игрока/персонажа
-        self.player_sprite = arcade.Sprite(
-            ":resources:images/animated_characters/female_person/femalePerson_idle.png",
-            PLAYER_SCALING,
-        )
+        # Map name
+        map_name = f"./data/levels/level_{self.level}.json"
 
-        # Стартовая позиция игрока/персонажа
-        self.player_sprite.center_x = 128
-        self.player_sprite.center_y = 64
-        self.player_list.append(self.player_sprite)
+        # Layer Specific Options for the Tilemap
+        layer_options = {
+            LAYER_NAME_PLATFORMS: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_SKY: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_GROUND: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_BACKGROUND: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_BACKGROUND2: {
+                "use_spatial_hash": True,
+            },
+            LAYER_NAME_DIE_BLOCK: {
+                "use_spatial_hash": True,
+            },
+        }
 
-        self.load_level(self.level)
+        # Load in TileMap
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
 
-        self.game_over = False
+        # Initiate New Scene with our TileMap, this will automatically add all layers
+        # from the map as SpriteLists in the scene in the proper order.
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
-    def load_level(self, level):
-        """Функция загрузки уровня"""
-        # layer_options = {"Platforms": {"use_spatial_hash": True}}
+        # Keep track of the score, make sure we keep the score if the player finishes a level
+        if self.reset_score:
+            self.score = 0
+        self.reset_score = True
 
-        # Считывает tiled map
-        self.tile_map = arcade.load_tilemap(
-            f":resources:tiled_maps/level_{level}.json", scaling=TILE_SPRITE_SCALING
-        )
+        # Add Player Spritelist before "Foreground" layer. This will make the foreground
+        # be drawn after the player, making it appear to be in front of the Player.
+        # Setting before using scene.add_sprite allows us to define where the SpriteList
+        # will be in the draw order. If we just use add_sprite, it will be appended to the
+        # end of the order.
+        self.scene.add_sprite_list_after("Player", LAYER_NAME_PLATFORMS)
 
-        # --- Стены ---
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = PLAYER_START_X
+        self.player_sprite.center_y = PLAYER_START_Y
+        self.scene.add_sprite("Player", self.player_sprite)
 
-        # Вычислияем правый край my_map в пикселях
+        # --- Load in a map from the tiled editor ---
+
+        # Calculate the right edge of the my_map in pixels
         self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
 
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite,
-            self.tile_map.sprite_lists["Platforms"],
-            gravity_constant=GRAVITY,
-        )
-
         # --- Other stuff
-        # Установка цвета фона
+        # Set the background color
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
 
-        # Установите границы окна просмотра
-        # Эти цифры указывают, куда мы «прокрутили».
-        self.view_left = 0
-        self.view_bottom = 0
-
-    def on_draw(self):
-        """Рендеринг экрана."""
-
-        self.frame_count += 1
-
-        # Эта команда должна выполнится до того, как мы начнем рисовать.
-        self.clear()
-
-        # Рисуем все спрайты.
-        self.player_list.draw()
-        self.tile_map.sprite_lists["Platforms"].draw()
-
-        if self.last_time and self.frame_count % 60 == 0:
-            fps = 1.0 / (time.time() - self.last_time) * 60
-            self.fps_message = f"FPS: {fps:5.0f}"
-
-        if self.fps_message:
-            arcade.draw_text(
-                self.fps_message,
-                self.view_left + 10,
-                self.view_bottom + 40,
-                arcade.color.BLACK,
-                14,
-            )
-
-        if self.frame_count % 60 == 0:
-            self.last_time = time.time()
-
-        # Размещаем текст на экран.
-        # Отрегулируйте положение текста в зависимости от области просмотра,
-        # чтобы не прокручивать текст.
-        distance = self.player_sprite.right
-        output = f"Distance: {distance:.0f}"
-        arcade.draw_text(
-            output, self.view_left + 10, self.view_bottom + 20, arcade.color.BLACK, 14
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite,
+            gravity_constant=GRAVITY,
+            walls=self.scene[LAYER_NAME_PLATFORMS],
         )
 
-        if self.game_over:
-            arcade.draw_text(
-                "Game Over",
-                self.view_left + 200,
-                self.view_bottom + 200,
-                arcade.color.BLACK,
-                30,
-            )
+    def on_draw(self):
+        """Render the screen."""
+
+        # Clear the screen to the background color
+        self.clear()
+
+        # Activate the game camera
+        self.camera.use()
+
+        # Draw our Scene
+        self.scene.draw()
+
+        # Activate the GUI camera before drawing GUI elements
+        self.gui_camera.use()
+
+        # Draw our score on the screen, scrolling it with the viewport
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(
+            score_text,
+            10,
+            10,
+            arcade.csscolor.BLACK,
+            18,
+        )
 
     def on_key_press(self, key, modifiers):
         """
         Вызывается при каждом движении мыши.
         """
-        if key == arcade.key.UP:
+        if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = JUMP_SPEED
-        elif key == arcade.key.LEFT:
+                arcade.play_sound(self.jump_sound)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """
         Вызывается, когда пользователь нажимает кнопку мыши.
         """
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+                self.camera.viewport_height / 2
+        )
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
 
     def on_update(self, delta_time):
         """Передвижение и игровая логика"""
 
-        if self.player_sprite.right >= self.end_of_map:
-            if self.level < self.max_level:
-                self.level += 1
-                self.load_level(self.level)
-                self.player_sprite.center_x = 128
-                self.player_sprite.center_y = 64
-                self.player_sprite.change_x = 0
-                self.player_sprite.change_y = 0
-            else:
-                self.game_over = True
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
-        # Вызоваем обновление для всех спрайтов (The sprites don't do much in this example though.)
-        #
-        if not self.game_over:
-            self.physics_engine.update()
+        # TODO дописать функцию монеток
+        # See if we hit any coins
+        # coin_hit_list = arcade.check_for_collision_with_list(
+        #     self.player_sprite, self.scene[LAYER_NAME_COINS]
+        # )
 
-        # --- Управление прокруткой ---
+        # Loop through each coin we hit (if any) and remove it
+        # for coin in coin_hit_list:
+        # Remove the coin
+        # coin.remove_from_sprite_lists()
+        # Play a sound
+        # arcade.play_sound(self.collect_coin_sound)
+        # Add one to the score
+        # self.score += 1
 
-        # Track if we need to change the view port
+        # Did the player fall off the map?
+        if self.player_sprite.center_y < -100:
+            self.player_sprite.center_x = PLAYER_START_X
+            self.player_sprite.center_y = PLAYER_START_Y
 
-        changed = False
+            arcade.play_sound(self.game_over)
 
-        # Прокрутка влево
-        left_bndry = self.view_left + VIEWPORT_LEFT_MARGIN
-        if self.player_sprite.left < left_bndry:
-            self.view_left -= left_bndry - self.player_sprite.left
-            changed = True
+        # Did the player touch something they should not?
+        if arcade.check_for_collision_with_list(
+                self.player_sprite, self.scene[LAYER_NAME_DIE_BLOCK]
+        ):
+            self.player_sprite.change_x = 0
+            self.player_sprite.change_y = 0
+            self.player_sprite.center_x = PLAYER_START_X
+            self.player_sprite.center_y = PLAYER_START_Y
 
-        # Прокрутка вправо
-        right_bndry = self.view_left + SCREEN_WIDTH - VIEWPORT_RIGHT_MARGIN
-        if self.player_sprite.right > right_bndry:
-            self.view_left += self.player_sprite.right - right_bndry
-            changed = True
+            arcade.play_sound(self.game_over)
 
-        # Прокрутка вверх
-        top_bndry = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN_TOP
-        if self.player_sprite.top > top_bndry:
-            self.view_bottom += self.player_sprite.top - top_bndry
-            changed = True
+        # See if the user got to the end of the level
+        if self.player_sprite.center_x >= self.end_of_map:
+            # Advance to the next level
+            self.level += 1
 
-        # Прокрутка вниз
-        bottom_bndry = self.view_bottom + VIEWPORT_MARGIN_BOTTOM
-        if self.player_sprite.bottom < bottom_bndry:
-            self.view_bottom -= bottom_bndry - self.player_sprite.bottom
-            changed = True
+            # Make sure to keep the score from this level when setting up the next level
+            self.reset_score = False
 
-        # Если нам нужно прокрутить, давайте сделаем это.
-        if changed:
-            self.view_left = int(self.view_left)
-            self.view_bottom = int(self.view_bottom)
-            arcade.set_viewport(
-                self.view_left,
-                SCREEN_WIDTH + self.view_left,
-                self.view_bottom,
-                SCREEN_HEIGHT + self.view_bottom,
-            )
+            # Load the next level
+            self.setup()
+
+        # Position the camera
+        self.center_camera_to_player()
 
 
 def main():
--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ SCREEN_TITLE = "Kind knight"  # название
 FULL_SCREEN = False  # полный экран
 
 # Размеры
-TILE_SPRITE_SCALING = 0.5  # размер плитки
-PLAYER_SCALING = 0.6  # размер игрока
+TILE_SCALING = 2  # размер плитки
+CHARACTER_SCALING = 0.7  # размер игрока
 SPRITE_PIXEL_SIZE = 128  # размер спрайтов
-GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SPRITE_SCALING  # Размер сетки
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING  # Размер сетки
 
 # Сколько пикселей оставить в качестве минимального поля между символами
 # и краем экрана.
@@ -21,3 +21,15 @@ VIEWPORT_LEFT_MARGIN = 270  # Лево
 MOVEMENT_SPEED = 5  # Скорость передвижения
 JUMP_SPEED = 23  # скорость прыжка
 GRAVITY = 1.1  # сила гравитации/притяжения
+
+# Player starting position
+PLAYER_START_X = 64
+PLAYER_START_Y = 225
+
+# Layer Names from our TileMap
+LAYER_NAME_SKY = "sky"
+LAYER_NAME_GROUND = "ground"
+LAYER_NAME_BACKGROUND = "BackGround"
+LAYER_NAME_BACKGROUND2 = "Background2"
+LAYER_NAME_PLATFORMS = "Platforms"
+LAYER_NAME_DIE_BLOCK = "DieBlocks"
