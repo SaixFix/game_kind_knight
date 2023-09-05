8ad5a84a40ab4828a93661d84618c9e6fe5d5f30
Maxim <108695003+SaixFix@users.noreply.github.com>
2023-09-05 10:09:17 +0300

    add textures

---
+++ data/levels/Gradient (z -6).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Gradient (z -6)" tilewidth="32" tileheight="32" tilecount="5797" columns="187">
+ <image source="Gradient (z -6).png" width="6000" height="1000"/>
+</tileset>
---
+++ data/levels/sky.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="mtn (z -4)" tilewidth="32" tileheight="32" tilecount="2793" columns="133">
+ <image source="mtn (z -4).png" width="4275" height="697"/>
+</tileset>
